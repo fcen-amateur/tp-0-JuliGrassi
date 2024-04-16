b4771f8469555b7712d31b5ca18d56a32fe7e64e
Julieta Grassi <109984584+JuliGrassi@users.noreply.github.com>
2024-04-16 15:40:05 -0300

    Update plot.py
    dejo código de paisesPorContinente

--- plotters/plot.py
+++ plotters/plot.py
@@ -17,29 +17,3 @@ def plot():
         autor="La cátedra",
         figura=figura,
     )
-import seaborn.objects as so
-from gapminder import gapminder
-
-
-def plot():
-    figura = (
-        so.Plot(
-            gapminder[gapminder.continent == "Oceania"],
-            x="year",
-            y="lifeExp",
-            color="country",
-        )
-        .add(so.Line())
-        .label(
-            title="Expectativa de vida en Oceanía",
-            x="Año",
-            y="Expectativa de vida",
-            color="País",
-        )
-    )
-    return dict(
-        descripcion="Expectativa de vida en países de Oceanía a lo largo del tiempo",
-        autor="La cátedra",
-        figura=figura,
-    )
-
